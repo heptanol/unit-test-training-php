bf1d70a58e0475df368a676dcc15ba4bb82825f1
heptanol <thamine.alaeddine@gmail.com>
2020-04-29 12:07:44 +0200

    Documentation php exercice

--- src/Email.php
+++ src/Email.php
@@ -1,9 +1,23 @@
 <?php
+/**
+ * @author Mohamed
+ * @version 1.1
+ */
 
+/**
+ * @class Email Email.php
+ */
 final class Email
 {
+    /**
+     * @attribut $mail string
+     */
     private $email;
 
+    /**
+     * Email constructor.
+     * @param string $email
+     */
     private function __construct(string $email)
     {
         $this->ensureIsValidEmail($email);
@@ -11,16 +25,26 @@ final class Email
         $this->email = $email;
     }
 
+    /**
+     * @param string $email
+     * @return Email
+     */
     public static function fromString(string $email): self
     {
         return new self($email);
     }
 
+    /**
+     * @return string
+     */
     public function __toString(): string
     {
         return $this->email;
     }
 
+    /**
+     * @param string $email
+     */
     private function ensureIsValidEmail(string $email): void
     {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
--- src/Salary.php
+++ src/Salary.php
@@ -1,8 +1,19 @@
 <?php
+/**
+ * @author Mohamed
+ * @version 1.1
+ */
 
-
+/**
+ * @class Salary
+ */
 class Salary
 {
+    /**
+     * Fonction calculNetSalary calcul le salaire net
+     * @param $grossMonthlySalary string salaire brut
+     * @return int salaire net
+     */
     static public function calculNetSalary($grossMonthlySalary)
     {
         return ($grossMonthlySalary * 55) / 100;
